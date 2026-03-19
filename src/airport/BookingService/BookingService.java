ed5f1cdfa81d8f12783d8adeaba3c1a36c64e711
Alekokakhidze17@gmail.com <alekokakhidze17@gmail.com>
2026-03-19 18:53:24 +0400

    Changes

---
+++ src/airport/BookingService/BookingService.java
@@ -0,0 +1,27 @@
+package airport.service;
+
+import airport.model.Passenger;
+import airport.model.Ticket;
+
+public class BookingService {
+
+    private static int bookingCount;
+
+    static {
+        bookingCount = 0;
+        System.out.println("BookingService initialized");
+    }
+
+    public static void bookSeat(Passenger passenger, Ticket ticket) {
+        bookingCount++;
+
+        System.out.println("Booking for: " + passenger.getName());
+        System.out.println("Seat: " + ticket.getSeatNumber());
+        System.out.println("Price: " + ticket.getPrice());
+        System.out.println("Email sent!");
+    }
+
+    public static int getBookingCount() {
+        return bookingCount;
+    }
+}
